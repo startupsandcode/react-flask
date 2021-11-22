a76a61a3720fcf357c1f39dc7ae81a230d5fa1dd
John Mann <jmann@sempre.ai>
2021-11-22 10:40:54 -0500

    updated api config

--- api/config.py
+++ api/config.py
@@ -1,7 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+from dotenv import load_dotenv
+load_dotenv(os.path.join(basedir, '.env.local'))
 
 class Config(object):
-    # ...
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:PASSWORD@localhost:3306/tuts'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
